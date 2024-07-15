347b6a8402192cbe47e471e2e6f274ca69a82ef6
takashu12 <k021c1348@g.neec.ac.jp>
2024-07-15 21:32:27 +0900

    20240715

--- firestore/app/build.gradle.kts
+++ firestore/app/build.gradle.kts
@@ -61,7 +61,7 @@ dependencies {
     implementation("com.google.firebase:firebase-firestore")
 
     // Firebase Authentication
-    implementation("com.google.firebase:firebase-auth:21.2.0")
+    implementation("com.google.firebase:firebase-auth")
 
     // Google Play services
     implementation("com.google.android.gms:play-services-auth:21.2.0")
